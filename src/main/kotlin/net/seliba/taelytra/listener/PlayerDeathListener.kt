470588463f9db8d19a8a7abcb9bbab0fb6d3a41e
Daniel Scherf <munita.gavai@gmail.com>
2020-10-10 21:31:39 +0200

    Increase the priority of the PlayerDeathListener

--- src/main/kotlin/net/seliba/taelytra/listener/PlayerDeathListener.kt
+++ src/main/kotlin/net/seliba/taelytra/listener/PlayerDeathListener.kt
@@ -2,13 +2,14 @@ package net.seliba.taelytra.listener
 
 import net.seliba.taelytra.utils.ItemStackComparator.Companion.isAboutSimilar
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.inventory.ItemStack
 
 class PlayerDeathListener(private val elytraItem: ItemStack) : Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     fun onPlayerDeath(event: PlayerDeathEvent) {
         event.drops.removeIf {
             itemStack: ItemStack? -> isElytraItem(itemStack)
