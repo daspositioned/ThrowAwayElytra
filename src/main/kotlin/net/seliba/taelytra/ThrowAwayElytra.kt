7697f95928ef73c708345166b4e9ac173a3f217f
das_ <git@heap.mozmail.com>
2020-10-11 13:37:59 +0200

    Add particles on landing, cleanup code

--- src/main/kotlin/net/seliba/taelytra/ThrowAwayElytra.kt
+++ src/main/kotlin/net/seliba/taelytra/ThrowAwayElytra.kt
@@ -6,6 +6,7 @@ import net.seliba.taelytra.listener.PlayerDeathListener
 import net.seliba.taelytra.scheduler.ElytraScheduler
 import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.bukkit.Particle
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.*
@@ -18,12 +19,13 @@ class ThrowAwayElytra : JavaPlugin() {
     private var regionName: String? = null
     private var elytraCheckInterval: Long? = null
     private var itemStack: ItemStack? = null
+    private var particle: Particle? = null
 
     override fun onEnable() {
         initializeConfig()
         parseConfig()
         startScheduler()
-        if (ElytraScheduler(this, elytraCheckInterval!!, permission, regionName!!, itemStack!!).start()) {
+        if (ElytraScheduler(this, elytraCheckInterval!!, permission, regionName!!, itemStack!!, particle).start()) {
             logger.info("Started successfully!")
         }
     }
@@ -36,6 +38,8 @@ class ThrowAwayElytra : JavaPlugin() {
         config.setDefault("permission.name", "throwawayelytra.use")
         config.setDefault("item.name", "&6Throw-Away Elytra")
         config.setDefault("item.lore", listOf("&aElytra which will &cdisappear &aafter", "&ayou &cland &aoutside of spawn"))
+        config.setDefault("on-landing.particle.enable", true)
+        config.setDefault("on-landing.particle.name", "CLOUD")
         config.save()
     }
 
@@ -43,6 +47,17 @@ class ThrowAwayElytra : JavaPlugin() {
         if (config.getBoolean("permission.enable")) {
             permission = config.getString("permission.name")
         }
+
+        if (config.getBoolean("on-landing.particle.enable")) {
+            try {
+                particle = Particle.valueOf(config.getString("on-landing.particle.name")!!.toUpperCase())
+            } catch(exception: IllegalArgumentException) {
+                logger.warning("Could not parse particle name!")
+                logger.warning("You can find a list of all particles on https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Particle.html")
+                logger.warning("Particles on landing will be disabled")
+            }
+        }
+
         regionName = config.getString("spawn-worldguard-region")
         elytraCheckInterval = config.getLong("elytra-check-interval-in-ticks")
 
